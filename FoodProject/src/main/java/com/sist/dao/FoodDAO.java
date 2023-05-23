6b8e37c67f141f1ef89295d47cda3aa186b536a5
SIST <SIST@DESKTOP-08E10UE>
2023-05-23 15:25:24 +0900

    1111

---
+++ FoodProject/src/main/java/com/sist/dao/FoodDAO.java
@@ -0,0 +1,192 @@
+package com.sist.dao;
+import java.sql.*;
+import java.util.*;
+import com.sist.vo.*;
+/*
+ *   1. 드라이버 등록
+ *      ------ 오라클 연결하는 라이브러리 (ojdbc8.jar)
+ *      OracleDriver => 메모리할당 
+ *   2. 오라클 연결 
+ *      Connection 
+ *   3. SQL문장을 전송 
+ *      PreparedStatement
+ *   4. SQL문장 실행 요청 
+ *      = executeUpdate() => INSERT , UPDATE , DELETE
+ *        --------------- COMMIT (AutoCommit)
+ *      = executeQuery() => SELECT
+ *        -------------- 결과값을 가지고 온다 
+ *                       -----
+ *                       ResultSet
+ *        ResultSet 
+ *         String sql="SELECT id,name,sex,age ";
+ *        ----------------------------
+ *           id     name   sex   age 
+ *        ----------------------------
+ *          aaa     홍길동   남자   20 | first() => next()
+ *                                    위치변경     위치변경후 데이터 읽기
+ *      getString(1) getString(2) getString(3) getInt(4)
+ *      getString("id") => mybatis 
+ *        ----------------------------
+ *          bbb     심청이   여자   23
+ *      getString(1) getString(2) getString(3) getInt(4)
+ *        ----------------------------
+ *          ccc     박문수   남자   27 | last() => previous()
+ *      getString(1) getString(2) getString(3) getInt(4)
+ *        ----------------------------
+ *         | 커서위치 
+ *   5. 닫기 
+ *      생성 반대로 닫는다 
+ *      rs.close() , ps.close(), conn.close() 
+ *  -------------------------- 오라클 연결 (Servlet => JSP)
+ *   
+ */
+public class FoodDAO {
+   //기능 => INSERT => 데이터수집 (파일)
+   private Connection conn;// 오라클 연결 객체 (데이터베이스 연결) 
+   private PreparedStatement ps;// SQL문장 전송 / 결과값 읽기
+   private final String URL="jdbc:oracle:thin:@localhost:1521:XE";
+   //  mySQL => jdbc:mysql://localhost/mydb
+   private static FoodDAO dao; // 싱글턴 패턴 
+   // DAO객체를 한개만 사용이 가능하게 만든다 
+   // 드라이버 설치 => 소프트웨어 (메모리 할당 요청) Class.forName()
+   // 클래스의 정보를 전송 
+   // 드라이버 설치는 1번만 수행 
+   public FoodDAO()
+   {
+	   try
+	   {
+		   Class.forName("oracle.jdbc.driver.OracleDriver");
+	   }catch(Exception ex){}
+   }
+   // 오라클 연결 
+   public void getConnection()
+   {
+	   try
+	   {
+		   conn=DriverManager.getConnection(URL,"hr","happy");
+		   // => 오라클 전송 : conn hr/happy
+	   }catch(Exception ex){}
+   }
+   // 오라클 연결 종료 
+   public void disConnection()
+   {
+	   try
+	   {
+		   if(ps!=null) ps.close();
+		   if(conn!=null) conn.close();
+		   // => 오라클 전송 : exit
+	   }catch(Exception ex){}
+   }
+   // DAO객체를 1개만 생성해서 사용 => 메모리 누수현상을 방지 (싱글턴 패턴)
+   // 싱글턴 / 팩토리 => 면접 (스프링:패턴 8개) 
+   public static FoodDAO newInstance()
+   {
+	   // newInstance() , getInstance() => 싱글턴 
+	   if(dao==null)
+		   dao=new FoodDAO();
+	   return dao;
+   }
+   // ============= 기본 셋팅 (모든 DAO)
+   // 기능 
+   // 1. 데이터 수집 (INSERT)
+   /*
+    *   Statement => SQL=> 생성과 동시에 데이터 추가 
+    *             "'"+name+"','"+sex+"','"+...
+    *   PreparedStatement => 미리 SQL문장을 만들고 나중에 값을 채운다 
+    *     => default 
+    *   CallableStatement => Procedure호출 
+    */
+   public void foodCategoryInsert(CategoryVO vo)
+   {
+	   try
+	   {
+		   //1. 연결 
+		   getConnection();
+		   //2. SQL문장 생성 
+		   String sql="INSERT INTO food_category VALUES("
+				     +"fc_cno_seq.nextval,?,?,?,?)";
+		   /*
+		    *    "'"+vo.getTitle()+"','"
+		    *    
+		    *    INSERT ~ VALUES('홍길동','서울'
+		    */
+		   //3. SQL문장을 오라클 전송 
+		   ps=conn.prepareStatement(sql);
+		   // 3-1 => ?에 값을 채운다 
+		   ps.setString(1, vo.getTitle());// "'"+vo.getTitle()+"'"
+		   ps.setString(2, vo.getSubject());
+		   ps.setString(3, vo.getPoster());
+		   ps.setString(4, vo.getLink());
+		   // 단점 => 번호가 잘못되면 오류 발생 , 데이터형이 다르면 오류 
+		   // IN,OUT ~
+		   //4. SQL문장을 실행 명령 => SQL문장을 작성하고 => 
+		   ps.executeUpdate();
+	   }catch(Exception ex) 
+	   {
+		   ex.printStackTrace();// 에러 확인 
+	   }
+	   finally
+	   {
+		   disConnection();// 오라클 연결 해제 => 무조건 
+	   }
+   }
+   // 2. SELECT => 전체 데이터 읽기 => 30개 (한개당 => CategoryVO)
+   // => Collection,배열 => 브라우저로 30개를 전송
+   // 브라우저 <==> 오라클 (X) 
+   // 브라우저 <==> 자바  <==> 오라클 => Spring Cloud 
+   // Collection , 메소드 제작 
+   public List<CategoryVO> foodCategoryData()
+   {
+	   List<CategoryVO> list=new ArrayList<CategoryVO>();
+	   try
+	   {
+		   //1. 오라클 연결
+		   getConnection();
+		   //2. SQL문장 
+		   String sql="SELECT cno,title,subject,poster "
+				     +"FROM food_category";
+		   //3. 오라클 전송
+		   ps=conn.prepareStatement(sql);
+		   //4. 실행후 결과값 받기
+		   ResultSet rs=ps.executeQuery();
+		   // rs에 있는 데이터를 list에 저장 
+		   while(rs.next())
+		   {
+			   CategoryVO vo=new CategoryVO();
+			   vo.setCno(rs.getInt(1));
+			   vo.setTitle(rs.getString(2));
+			   vo.setSubject(rs.getString(3));
+			   String poster=rs.getString(4);
+			   poster=poster.replace("#", "&");
+			   vo.setPoster(poster);
+			   
+			   list.add(vo);
+		   }
+		   rs.close();
+	   }catch(Exception ex)
+	   {
+		   ex.printStackTrace();
+	   }
+	   finally
+	   {
+		   // 오라클 닫기 
+		   disConnection();
+	   }
+	   return list;
+   }
+   // 3. 상세보기 => WHERE 
+   
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ FoodProject/src/main/webapp/food/main.jsp
@@ -0,0 +1,97 @@
+<%@page import="com.sist.vo.CategoryVO"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" import="java.util.*,com.sist.dao.*"%>
+<%
+    // 자바가 들어가는 위치 
+    FoodDAO dao=FoodDAO.newInstance();
+    List<CategoryVO> list=dao.foodCategoryData();
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<style type="text/css">
+.container{
+   margin-top: 50px;
+}
+.row {
+  width: 100%;
+  margin: 0px auto;
+}
+</style>
+</head>
+<body>
+   <div class="container">
+     <h1 style="color:orange">믿고보는 맛집 리스트</h1>
+     <div class="row">
+       <%
+          for(int i=0;i<12;i++)
+          {
+        	  CategoryVO vo=list.get(i);
+       %>
+              <div class="col-md-3">
+			    <div class="thumbnail">
+			      <a href="#">
+			        <img src="<%=vo.getPoster()%>" 
+			           title="<%=vo.getSubject() %>" style="width:100%">
+			        <div class="caption">
+			          <p><%=vo.getTitle() %></p>
+			        </div>
+			      </a>
+			    </div>
+			  </div>
+       <%
+          }
+       %>
+     </div>
+     
+     <h1 style="color:orange">지역별 인기 맛집</h1>
+     <div class="row">
+       <%
+          for(int i=12;i<18;i++)
+          {
+        	  CategoryVO vo=list.get(i);
+       %>
+              <div class="col-md-4">
+			    <div class="thumbnail">
+			      <a href="#">
+			        <img src="<%=vo.getPoster()%>" 
+			           title="<%=vo.getSubject() %>" style="width:100%">
+			        <div class="caption">
+			          <p><%=vo.getTitle() %></p>
+			        </div>
+			      </a>
+			    </div>
+			  </div>
+       <%
+          }
+       %>
+     </div>
+     
+     <h1 style="color:orange">메뉴별 인기 맛집</h1>
+     <div class="row">
+       <%
+          for(int i=18;i<30;i++)
+          {
+        	  CategoryVO vo=list.get(i);
+       %>
+              <div class="col-md-3">
+			    <div class="thumbnail">
+			      <a href="#">
+			        <img src="<%=vo.getPoster()%>" 
+			           title="<%=vo.getSubject() %>" style="width:100%">
+			        <div class="caption">
+			          <p><%=vo.getTitle() %></p>
+			        </div>
+			      </a>
+			    </div>
+			  </div>
+       <%
+          }
+       %>
+     </div>
+   </div>
+</body>
+</html>
